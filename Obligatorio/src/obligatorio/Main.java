ae2751b21fb064502a6e0824b142128fd90980a2
Aparicio Braganca <apraricio.braganca@gmail.com>
2016-10-27 11:50:42 -0300

    Creando instancia de ventana principal y sistema

--- Obligatorio/src/obligatorio/Main.java
+++ Obligatorio/src/obligatorio/Main.java
@@ -1,12 +1,17 @@
 
 package obligatorio;
 
+import Interfaz.VentanaPrincipal;
+
 
 public class Main {
 
     
     public static void main(String[] args) {
         // TODO code application logic here
+        Sistema s = new Sistema();
+        VentanaPrincipal miVentana = new VentanaPrincipal(s);
+        miVentana.setVisible(true);
     }
     
 }
