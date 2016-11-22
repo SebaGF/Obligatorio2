41f12be522fa60b4119160557c2322997869e8ff
Seba Garcia <garciaseba96.sg@gmail.com>
2016-11-22 17:26:34 -0300

    Metodos en RealizarSorteo

--- Obligatorio/src/interfaz/RealizarSorteo.java
+++ Obligatorio/src/interfaz/RealizarSorteo.java
@@ -27,14 +27,17 @@ public class RealizarSorteo extends javax.swing.JPanel {
         initComponents();
         miSistema = s;
         ImageIcon iconoConfir = new ImageIcon(getClass().getResource("/Imagenes/confirmar.png"));
+        ImageIcon iconoError = new ImageIcon(getClass().getResource("/Imagenes/error.png"));
+        ImageIcon error = new ImageIcon(iconoError.getImage().getScaledInstance(22, -1, Image.SCALE_DEFAULT));
         ImageIcon confirmar = new ImageIcon(iconoConfir.getImage().getScaledInstance(50, -1, Image.SCALE_DEFAULT));
         btnSortear.setIcon(confirmar);
+        lblError.setIcon(error);
+        lblError.setVisible(false);
         cargarSorteos();
-        lstGanadores.setVisible(false);
-        
+        pnlGanadores.setVisible(false);
+
     }
 
-   
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -49,6 +52,7 @@ public class RealizarSorteo extends javax.swing.JPanel {
         jLabel4 = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
         lstGanadores = new javax.swing.JList();
+        lblError = new javax.swing.JLabel();
 
         jLabel2.setText("jLabel2");
 
@@ -73,7 +77,7 @@ public class RealizarSorteo extends javax.swing.JPanel {
                 btnSortearActionPerformed(evt);
             }
         });
-        add(btnSortear, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 120, 50, 40));
+        add(btnSortear, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 110, 70, 70));
 
         jLabel4.setText("Ganadores:");
 
@@ -86,9 +90,9 @@ public class RealizarSorteo extends javax.swing.JPanel {
             .addGroup(pnlGanadoresLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel4)
-                .addGap(18, 18, 18)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 225, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(21, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         pnlGanadoresLayout.setVerticalGroup(
             pnlGanadoresLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -100,21 +104,29 @@ public class RealizarSorteo extends javax.swing.JPanel {
                 .addContainerGap(19, Short.MAX_VALUE))
         );
 
-        add(pnlGanadores, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 200, 330, 160));
+        add(pnlGanadores, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 180, 330, 160));
+        add(lblError, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 40, 40, 40));
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnSortearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSortearActionPerformed
-        if(lstSorteos.getSelectedIndex()>-1){
-            Sorteo s = new Sorteo();
-            s = miSistema.getLstSorteos().get(lstSorteos.getSelectedIndex());
+        ArrayList<Evaluacion> aSortear = new ArrayList();
+        Sorteo s = new Sorteo();
+        s = miSistema.getLstSorteos().get(lstSorteos.getSelectedIndex());
+        listaParaSortear(aSortear, s);
+        
+        if (lstSorteos.getSelectedIndex() > -1 && aSortear.size()>=s.getCantGanadores()) {
             Evaluacion[] ganadores = new Evaluacion[s.getCantGanadores()];
             ganadores = obtenerGanadores(s.getCantGanadores(), s);
             cargarGanadores(ganadores);
-            lstGanadores.setVisible(true);
-            
-           
+            pnlGanadores.setVisible(true);
+            miSistema.getLstSorteos().remove(s);
+            lblError.setVisible(false);
+            cargarSorteos();
+
+        }else{
+            lblError.setVisible(true);
         }
-        
+
     }//GEN-LAST:event_btnSortearActionPerformed
 
 
@@ -126,6 +138,7 @@ public class RealizarSorteo extends javax.swing.JPanel {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JLabel lblError;
     private javax.swing.JList lstGanadores;
     private javax.swing.JList lstSorteos;
     private javax.swing.JPanel pnlGanadores;
@@ -140,38 +153,66 @@ public void cargarSorteos() {
         lstSorteos.setModel(modelo);
     }
 
-public Evaluacion[] obtenerGanadores(int num, Sorteo s){
-    ArrayList<Evaluacion> aSortear = new ArrayList();
-    Evaluacion[] ganadores = new Evaluacion[num];
-    Random ran = new Random();
-    int [] cant = new int [num];
-    
-    for (int i = 0; i < miSistema.getLstEvaluacionSorteo().size(); i++) {
-        if(miSistema.comparaFechas(s.getFechaInicio(), miSistema.getLstEvaluacionSorteo().get(i).getFecha()) 
-                && miSistema.comparaFechas(miSistema.getLstEvaluacionSorteo().get(i).getFecha(), s.getFechaCierre())){
-            System.out.println("entroooo");
-            aSortear.add(miSistema.getLstEvaluacionSorteo().get(i));
-        }
-    }
-    for (int i = 0; i < cant.length; i++) {
-        int a = ran.nextInt(aSortear.size());
-        cant[i] = a ;
-    }
-    for (int i = 0; i < ganadores.length; i++) {
-        ganadores[i] = aSortear.get(cant[i]);
-    }
-    JOptionPane.showMessageDialog(null, ganadores);
-    
-    return ganadores;
-}
+    public Evaluacion[] obtenerGanadores(int num, Sorteo s) {
+        ArrayList<Evaluacion> aSortear = new ArrayList();
+        Evaluacion[] ganadores = new Evaluacion[num];
+        Random ran = new Random();
+        int[] cant = new int[num];
+                    
+        listaParaSortear(aSortear, s);
 
-public void cargarGanadores(Evaluacion[] e){
-    DefaultListModel modelo = new DefaultListModel();
-       for (int i = 0; i < e.length; i++) {
+        for (int i = 0; i < cant.length; i++) {
+            cant[i] = -1;
+        }
+
+        for (int i = 0; i < cant.length; i++) {
+
+            int a = ran.nextInt(aSortear.size());
+
+            while (yaEsta(cant, a)) {
+                a = ran.nextInt(aSortear.size());
+            }
+
+            cant[i] = a;
+
+        }
+
+        for (int i = 0; i < ganadores.length; i++) {
+            ganadores[i] = aSortear.get(cant[i]);
+        }
+
+        return ganadores;
+    }
+
+    public void cargarGanadores(Evaluacion[] e) {
+        DefaultListModel modelo = new DefaultListModel();
+        for (int i = 0; i < e.length; i++) {
             modelo.addElement(e[i]);
         }
         lstGanadores.setModel(modelo);
-}
+    }
+
+    public boolean yaEsta(int[] a, int b) {
+
+        boolean ok = false;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == b) {
+                ok = true;
+            }
+        }
+        return ok;
+    }
+    
+    public void listaParaSortear(ArrayList<Evaluacion> aSortear, Sorteo s){
+        
+        for (int i = 0; i < miSistema.getLstEvaluacionSorteo().size(); i++) {
+            if (miSistema.comparaFechas(s.getFechaInicio(), miSistema.getLstEvaluacionSorteo().get(i).getFecha())
+                    && miSistema.comparaFechas(miSistema.getLstEvaluacionSorteo().get(i).getFecha(), s.getFechaCierre())) {
+                aSortear.add(miSistema.getLstEvaluacionSorteo().get(i));
+            }
+        }
+    }
+    
     Sistema miSistema;
 
 }
